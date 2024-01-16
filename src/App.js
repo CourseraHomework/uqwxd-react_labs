6ef9d4baa9de73d01ecbe1377b26fcfa0d5fd2dd
Coursera Homework <courserahomework@pyrophoenix.com>
2024-01-15 20:58:21 -0500

    changes for the practice project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Code to import Budget.js
 import Budget from './components/Budget';
 
+//Code to import Remaining.js
+import Remaining from './components/Remaining';
+
+//Code to import ExpenseTotal.js
+import ExpenseTotal from './components/ExpenseTotal';
+
+//Code to import ExpenseList.js
+import ExpenseList from './components/ExpenseList';
+
+//Code to import ExpenseItem.js
+import ExpenseItem from './components/ExpenseItem';
+
+//Code to import AllocationForm.js
+import AllocationForm from './components/AllocationForm';
+
+
 // Add code to import the other components here under
 
 
@@ -14,30 +30,54 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    {
+                        /* Add Budget component here */
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
+                    }
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    {
+                        /* Add Remaining component here*/
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
+                    }
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                    {
+                        /* Add ExpenseTotal component here */
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                    }
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                    {
+                        /* Add ExpenseList component here */
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                        /* Add ExpenseTotal component here */
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>
+                    }
+
+                    {
+                        /* Add ExpenseItem component here */
+
+                        /* Add ExpenseTotal component here */
+                        <div className='col-sm'>
+                            <ExpenseItem />
+                        </div>
+                    }
+
+                    {
+                        /* Add AllocationForm component here under */
+
+                        /* Add ExpenseTotal component here */
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>
+                    }
 
                 </div>
             </div>
